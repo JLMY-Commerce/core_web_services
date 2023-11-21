0f93d4d9ed82d0dc7ab952565a76d03e05db9496
Yulia Rayane Rodrigues <yuliarodrigues@aluno.unifapce.edu.br>
2023-11-21 16:47:16 -0300

    Fix static and media URL configuration

--- core/urls.py
+++ core/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("docs/", include("docs.urls")),
     path('contas/', include("accounts.urls")),
-
     path('', include("web.urls"))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
